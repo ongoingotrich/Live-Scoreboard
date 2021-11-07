5aa6ac52816ef4b7f0a6c5a63196a65992c24eb7
FeSmash <65754161+ongoingotrich@users.noreply.github.com>
2021-11-06 21:58:57 -0600

    Add files via upload

--- docs/control.js
+++ docs/control.js
@@ -187,4 +187,48 @@ pickr.on('change', (color, source, instance) => {
 })
 
 
+// Simple example, see optional options for more configuration.
+const pickr2 = Pickr.create({
+    el: '.color-picker',
+    theme: 'classic', // or 'monolith', or 'nano'
+    default: 'rgba(255, 17, 0, 1)',
+
+    swatches: [
+        'rgba(255, 17, 0, 1)',
+        'rgba(0, 81, 255, 1)',
+        'rgba(111, 0, 255, 1)',
+        'rgba(255, 186, 0, 1)',
+        'rgba(70, 87, 176, 1)',
+        'rgba(33, 150, 243, 0.75)',
+        'rgba(3, 169, 244, 0.7)',
+        'rgba(0, 188, 212, 0.7)',
+        'rgba(0, 150, 136, 0.75)',
+        'rgba(76, 175, 80, 0.8)',
+        'rgba(139, 195, 74, 0.85)',
+        'rgba(205, 220, 57, 0.9)',
+        'rgba(255, 235, 59, 0.95)',
+        'rgba(255, 193, 7, 1)'
+    ],
+
+    components: {
+
+        // Main components
+        preview: true,
+        opacity: true,
+        hue: true,
+
+        // Input / output Options
+        interaction: {
+            hex: false,
+            rgba: true,
+            hsla: false,
+            hsva: false,
+            cmyk: false,
+            input: true,
+           
+        }
+    }
+});
+
+
 
